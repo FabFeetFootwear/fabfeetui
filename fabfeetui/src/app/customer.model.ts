cc7bd639670e7a640d142447c1fd106b9316931d
aharnalaha98 <aharnalaha98@gmail.com>
2021-03-18 21:19:39 +0530

    error deleted

--- fabfeetui/src/app/customer.model.ts
+++
@@ -1,9 +0,0 @@
-export class Customer {
-    constructor(public cid?:string,
-        public cname?:string,
-        public username?:string,
-        public password?:string,
-        public email?:string,
-        public phone?:number,
-        public address?:string){}
-}
