cfcc35f0764695fc7c0cdc661f9b68b5cf7a9e79
manvishah <shahmanvi123@gmail.com>
2021-03-21 15:35:00 +0530

    changes in angular

--- fabfeetui/src/app/supplier.model.ts
+++
@@ -1,9 +0,0 @@
-export class Supplier {
-    constructor(public sid?:string,
-        public sname?:string,
-        public username?:string,
-        public password?:string,
-        public address?:string,
-        public phone?:number
-        ){}
-}
